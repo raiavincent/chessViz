e8abf28b157ad3d8ce1ddefd92408a41306d2e16
Vincent Raia <vincentraia1@gmail.com>
2022-12-23 19:12:55 -0500

    fix to_gbq
    change .to_gbq if_exists to 'replace' from 'append'

--- dataUpload.py
+++ dataUpload.py
@@ -11,18 +11,22 @@ from puzzleCredentials import table_id
 from puzzleCredentials import project_id
 import warnings
 
+# data does not live in the same location as my code, need to direct the code to that location
 filePath = r"C:\Users\vince\OneDrive\Chess\training - v2.xlsx"
 
 print('Locating data.')
 
+# using the xlsx_table function from pyjanitor to extract my data
 puzzleHistory = xlsx_table(filePath, sheetname='table', table='puzzleScoring')
 
 print('Data located.')
 
+# a package has a deprecation warning I would like to temporarily ignore
 warnings.filterwarnings("ignore", category=DeprecationWarning) 
 
+# housing data in BigQuery, using pandas_gbq to facilitate the uploads
 pandas_gbq.to_gbq(
-    puzzleHistory, table_id, project_id=project_id, if_exists='append',
+    puzzleHistory, table_id, project_id=project_id, if_exists='replace',
     table_schema=table_schema)
 
 print('Data uploaded.')
