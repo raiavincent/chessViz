d1147ec11bafea996e8e8fd5ae591d0a114443a9
Vincent Raia <vincentraia1@gmail.com>
2022-12-23 21:09:37 -0500

    update puzzleCleanView for weekCode

--- sql/puzzleCleanView.sql
+++ sql/puzzleCleanView.sql
@@ -6,6 +6,7 @@ SELECT
   EXTRACT(year FROM date) AS year,
   EXTRACT(month FROM date) AS month,
   EXTRACT(week FROM date) AS week,
-  CONCAT(EXTRACT(year FROM date),"-",EXTRACT(month FROM date)) AS dateCode
+  CONCAT(EXTRACT(year FROM date),"-",EXTRACT(month FROM date)) AS dateCode,
+  CONCAT(EXTRACT(week FROM date),"-",EXTRACT(month FROM date)) AS weekCode
 FROM 
   `chess-371023.chessPuzzles.puzzleHistory`
