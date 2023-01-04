0064ab7960ec3c11867de8e89ab2521b77ff2521
Vincent Raia <vincentraia1@gmail.com>
2023-01-03 20:07:40 -0500

    Update randomPuzzle.py

--- randomPuzzle.py
+++ randomPuzzle.py
@@ -3,6 +3,10 @@ import pandas as pd
 import pandas_gbq
 import random
 import requests
+import tkinter as tk
+from tkinter import messagebox
+
+root = tk.Tk()
 
 pull = '''SELECT * FROM `chess-371023.chessPuzzles.puzzlesClean`'''
 
@@ -30,4 +34,22 @@ def getPuzzle():
 
     print(data.loc[data['puzzleID'] == randomPuzzle])
 
-getPuzzle()
+    canvas1.insert(data.loc[data['puzzleID'] == randomPuzzle])
+
+canvas1 = tk.Canvas(root, width=300, height=300)
+canvas1.pack()
+
+# def exit_application():
+#     msg_boxRun = tk.messagebox.askquestion('Puzzle?'
+#                                         icon='warning')
+#     if msg_boxRun == 'yes':
+#         root.destroy()
+#     else:
+#         tk.messagebox.showinfo('Return', 'You will now return to the application screen')
+
+button1 = tk.Button(root, text='Puzzle?', command=getPuzzle, bg='brown', fg='white')
+canvas1.create_window(150, 150, window=button1)
+
+root.mainloop()
+
+# getPuzzle()
